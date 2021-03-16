941bcab482670d18891f1a1e7ea307858a80ed49
w27 <phatsvan@gmail.com>
2021-03-16 19:34:21 +0300

    fix and test

---
+++ tests/test_bubble_sort.py
@@ -0,0 +1,12 @@
+# import pytest
+# from tasks.bubble_sort import bubble_sort
+#
+#
+# @pytest.mark.parametrize("value, rez", [
+#     ([0, 2, 5, 6, -1], [-1, 0, 2, 5, 6]),
+#     ([1, 1, 1, 1, 1], [1, 1, 1, 1, 1]),
+#     ([], [])
+# ])
+# def test_bubble_sort_good(value, rez):
+#     assert rez == bubble_sort(value)
+#
